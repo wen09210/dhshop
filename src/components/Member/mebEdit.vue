f6f37080ba229d09da433f997283dbd31d21a0b8
lee <@lee.ll559@gmail.com>
2017-04-15 13:54:12 +0800

    add icon

---
+++ src/components/Member/mebEdit.vue
@@ -0,0 +1,28 @@
+<form class="form-horizontal" role="form">
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">電子郵件</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="輸入電子郵件">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">密碼</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="密碼">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> 記住我
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">登入</button>
+    </div>
+  </div>
+</form>
--- src/components/Member/mebLogin.vue
+++ src/components/Member/mebLogin.vue
@@ -1,24 +1,55 @@
 <template>
-<div class="container">
-  <form role="form">
-    <div class="form-group">
-      <label for="account">帳號</label>
-      <input type="email" class="form-control" id="account" placeholder="輸入帳號">
+  <div class="container row">
+    <div class="col-md-4">
+      <form role="form">
+        <div class="form-group">
+          <img src="../../assets/icon/dhshop_logo.svg" class="img-responsive" alt="Responsive image">
+        </div>
+        <div class="form-group">
+          <label for="account">帳號</label>
+          <input type="email" class="form-control" id="account" placeholder="輸入帳號">
+        </div>
+        <div class="form-group">
+          <label for="pwd">密碼</label>
+          <input type="Password" class="form-control" id="pwd" placeholder="密碼">
+        </div>
+        <div class="checkbox">
+          <label>
+             <input type="checkbox"> 保持登入，除非點選登出鍵。
+          </label>
+        </div>
+        <button type="button" class="btn btn-info ">送出</button>
+      </form>
+      <p class="help-block">忘記密碼 | 註冊會員。</p>
     </div>
-    <div class="form-group">
-      <label for="pwd">密碼</label>
-      <input type="Password" class="form-control" id="pwd" placeholder="密碼">
+
+    <div class="col-md-4 ">
+      <div class="form-group">
+      </div>
+      <div class="form-group">
+        <img src="../../assets/icon/login_google.png" class="img-responsive" alt="Responsive image">
+      </div>
+      <div class="form-group">
+        <img src="../../assets/icon/login_fb.png" class="img-responsive" alt="Responsive image">
+      </div>
+      <div class="form-group">
+        <img src="../../assets/icon/login_yahoo.png" class="img-responsive" alt="Responsive image">
+      </div>
     </div>
-    <button type="button" class="btn btn-default">送出</button>
-  </form>
-</div>
+  </div>
 </template>
+
+
 <script>
   export default {}
 </script>
+
 <style scoped>
   div .form-control {
-    width: 450px;
+    width: 250px;
+  }
+  div .btn {
+    width: 250px;
   }
 </style>
 
