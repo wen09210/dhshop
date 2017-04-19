d967e52052e26c0e63b3f01316eeb13d7a1a2445
Lee <lee.ll559@gmail.com>
2017-04-20 01:42:59 +0800

    change cart

--- src/components/Cart/cartProd.vue
+++ src/components/Cart/cartProd.vue
@@ -1,76 +1,9 @@
 <template>
   <div>
-    <h4>購物車</h4>
-    <ul class="col-xs-4">
-      <li v-for="item in GetProductList">
-        <div>{{item.name}}</div>
-        <div>{{item.count | test}}</div>
-        <img :src="item.src"
-             style="width:200px;height:100px;">
-        <button class="btn btn-danger"
-                @click="IncreaseProduct(item)">+</button>
-        <span></span>
-        <button class="btn btn-danger"
-                @click="ReduceProduct(item)">-</button>
-      </li>
-    </ul>
-    <ul class="col-xs-4">
-      <li v-for="item in GetShoppingCartItem">
-        <span>{{item.name}}</span>
-        <span>{{item.count}}</span>
-      </li>
-    </ul>
-    <router-link class="btn btn-default btn-lg" to='/cartPay'>結賬</router-link>
+   
   </div>
 </template>
 
 <script>
-import {
-  mapActions,
-  mapGetters
-} from 'vuex'
-
-class ProductItem {
-  constructor (name, count, totalPern, src) {
-    this.name = name
-    this.count = count
-    this.totalPern = totalPern
-    this.src = src
-  }
-  getRandomInt (min, max) {
-    let nmin = Math.ceil(min)
-    let nmax = Math.floor(max)
-    return Math.floor(Math.random() * (nmax - nmin)) + nmin
-  }
-}
-const imgList = ['http://www.dhshop.tw/img/product/mask.jpg', 'http://dhshop.tw/img/product/bottle.jpg', 'http://yesim.tw/img/product/gluedots.jpg', 'http://yesim.tw/img/product/pokemon_ball.png']
-
-export default {
-  created () {
-    this.ClearProductList()
-    for (var i = 0; i <= 3; i++) {
-      var pro = new ProductItem()
-      pro.name = `Item${i}`
-      pro.count = `${pro.getRandomInt(100, 10000).toString()}`
-      pro.src = imgList[i]
-      this.GetProductList.push(pro)
-    }
-  },
-  mounted () {
-  },
-  computed: {
-    ...mapGetters([
-      'GetProductList',
-      'GetShoppingCartItem'
-    ])
-  },
-  methods: {
-    ...mapActions([
-      'IncreaseProduct',
-      'ReduceProduct',
-      'ClearShoppingCartItem',
-      'ClearProductList'
-    ])
-  }
-}
+export default{}
 </script>
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,9 +3,10 @@ import types from './types'
 export default {
   [types.IncreaseProduct] (state, item) {
     state.shoppingCartItem.push(item)
+    console.log(state.shoppingCartItem)
   },
-  [types.ReduceProduct] (state) {
-    return state.productItem
+  [types.ReduceProduct] (state, item) {
+    state.shoppingCartItem.pop(item)
   },
   [types.ClearShoppingCartItem] (state) {
     state.shoppingCartItem = []
